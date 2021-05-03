9a3f26978bf98c26b2303a34ba6e46652a9a16da
JuliCarracedo <julicarra@hotmail.com>
2021-05-03 16:46:18 -0300

    Add DRY philosophy

--- bin/main.rb
+++ bin/main.rb
@@ -1,13 +1,22 @@
 #!/usr/bin/env ruby
 
-# rubocop:disable Metrics/BlockNesting
-# rubocop:disable Style/IdenticalConditionalBranches
+def check_name
+  status = false
+  player = ''
+  while status == false
+    player = gets.chomp
+    status = true unless player.empty?
+    puts 'Please insert a valid name' unless status
+  end
+  player
+end
 
-puts "WELCOME TO JULIAN AND DAVID'S TIC-TAC-TOE"
-puts "Player 1, what's your name?"
-p1 = gets.chomp
-puts "Player 2, what's your name?"
-p2 = gets.chomp
+puts 'WELCOME TO JULIAN AND DAVID\'S TIC-TAC-TOE'
+puts 'Player 1, what\'s your name?'
+p1 = check_name
+
+puts 'Player 2, what\'s your name?'
+p2 = check_name
 
 def clear
   if Gem.win_platform?
@@ -18,46 +27,36 @@ def clear
 end
 
 puts "#{p1} will play Xs. #{p2} will play Os"
+
 i = 0
 input = 0
 status = false
-while i < 9
 
+while i < 9
   status = false
 
-  board = '+---+---+---+
-| 1 | 2 | 3 |
-+---+---+---+
-| 4 | 5 | 6 |
-+---+---+---+
-| 7 | 8 | 9 |
-+---+---+---+'
+  board = '   +---+---+---+
+    | 1 | 2 | 3 |
+    +---+---+---+
+    | 4 | 5 | 6 |
+    +---+---+---+
+    | 7 | 8 | 9 |
+    +---+---+---+'
   puts board
 
-  if i.even?
-    puts "#{p1}. Make your move. Select an available spot in the board"
-    while status == false
-      input = gets.chomp
-      status = true if (1..9).include?(input.to_i)
-      puts 'Invalid move. Please use an available' unless status
-      puts "Your move was #{input}" if status
-    end
-  else
-    puts "#{p2}. Make your move. Select an available spot in the board" unless i.even?
-    while status == false
-      input = gets.chomp
-      status = true if (1..9).include?(input.to_i)
-      puts 'Invalid move. Please use an available' unless status
-      puts "Your move was #{input}" if status
-    end
+  puts "#{p1}. Make your move. Select an available spot in the board" if i.even?
+  puts "#{p2}. Make your move. Select an available spot in the board" unless i.even?
+  while status == false
+    input = gets.chomp
+    status = true if (1..9).include?(input.to_i)
+    puts 'Invalid move. Please use an available' unless status
+    puts "Your move was #{input}" if status
   end
-
   clear
 
   i += 1
 end
 
-puts "IT'S A TIE"
-
-# rubocop:enable Metrics/BlockNesting
-# rubocop:enable Style/IdenticalConditionalBranches
+puts 'IT\'S A TIE'
+puts "#{p1} You WIN!"
+puts "#{p2} You WIN!"
