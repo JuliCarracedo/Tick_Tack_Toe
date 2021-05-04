d9793b5f8d1b031676109dbf44233b90e900fc58
petumazo <davidalvarezmazzo@gmail.com>
2021-05-04 09:52:49 -0500

    Remove puts comand in player.rb

--- lib/player.rb
+++ lib/player.rb
@@ -1,11 +1,10 @@
 class Player 
 
-    attr_reader :name
+    attr_reader :name,
     attr_reader :token
     @@instances = 0
 
     def initialize (name)
-        puts 'Only create 2 players' if @@instances > 1
         return nil if @@instances > 1 
         @name = name
         @token = @@instances == 0 ? 'X' : 'O' 
