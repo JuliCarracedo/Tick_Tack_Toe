2d84bcdbdcb3f679f3f838443d4311299252c2d1
panda4cs <panda4cs@gmail.com>
2021-05-11 00:04:28 +0200

    Add test for #valid_name in Judge class

--- spec/judge_spec.rb
+++ spec/judge_spec.rb
@@ -2,6 +2,12 @@ require_relative '../lib/judge'
 
 describe Player do
   let(:judge) { Judge.new }
+  describe '#valid_name' do
+    it 'returns wither a move is valid on board' do
+      expect(judge.valid_name('')).to eql(false)
+      expect(judge.valid_name('Omar')).to eql('Omar')
+    end
+  end
   describe '#valid_move' do
     it 'returns wither a move is valid on board' do
       expect(judge.valid_move(1, [2..8])).to eql(1)
