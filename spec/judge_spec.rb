44c9e87f2baf0b5bb9fc4996d3c9a96362246c79
panda4cs <panda4cs@gmail.com>
2021-05-11 00:00:49 +0200

    Add test for Judge class

---
+++ spec/judge_spec.rb
@@ -0,0 +1,11 @@
+require_relative '../lib/judge'
+
+describe Player do
+  let(:judge) { Judge.new }
+  describe '#valid_move' do
+    it 'returns wither a move is valid on board' do
+      expect(judge.valid_move(1, [2..8])).to eql(1)
+      expect(judge.valid_move(-3, [2..8])).to eql(false)
+    end
+  end
+end
